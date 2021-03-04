6b4b9fd27dc17b6f580f41e4c23a4182668f301f
nazeudon <nazeudon48112@icloud.com>
2021-03-04 09:20:31 +0900

    add vector

--- 8_vector/src/main.rs
+++ 8_vector/src/main.rs
@@ -12,4 +12,36 @@ fn main() {
     v3.push(7);
     v3.push(8);
     println!("The value of v3: {:?}", v3);
+
+    let v4 = vec![1, 2, 3, 4];
+    let third: &i32 = &v4[2];
+    let third2: Option<&i32> = v.get(2);
+    println!("The value of v4: {:?}", v4);
+    println!("The value of third: {:?}", third);
+    println!("The value of third2: {:?}", third2);
+
+    let v5 = vec![100, 32, 57];
+    for i in &v5 {
+        println!("{}", i);
+    }
+    println!("The value of v5: {:?}", v5);
+
+    let mut v6 = vec![100, 32, 54];
+    for i in &mut v6 {
+        *i += 50;
+    }
+    println!("The value of v6: {:?}", v6);
+
+    #[derive(Debug)]
+    enum SpreadsheetCell {
+        Int(i32),
+        Float(f64),
+        Text(String),
+    }
+    let row = vec![
+        SpreadsheetCell::Int(3),
+        SpreadsheetCell::Float(10.12),
+        SpreadsheetCell::Text(String::from("blue")),
+    ];
+    println!("The value of row: {:?}", row);
 }
